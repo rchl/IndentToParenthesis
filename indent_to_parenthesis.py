5243438071240399ab62a7b2fe41724c41d48a6b
Rafal Chlodnicki <rchlodnicki@opera.com>
2015-09-19 23:52:00 +0200

    Remove all spaces around cursor before aligning to parenthesis
    On enter, all spaces around cursor are now removed before aligning
    trailing part of the line to parenthesis. Makes it unecessary to do
    it manually before pressing enter.

--- indent_to_parenthesis.py
+++ indent_to_parenthesis.py
@@ -7,17 +7,20 @@ class IndentToParenthesisCommand(sublime_plugin.TextCommand):
     view = self.view
     selections = view.sel()
     for selection in selections:
-      line_to_cursor = view.substr(
+      line_upto_cursor = view.substr(
           sublime.Region(view.line(selection).a, selection.b))
-      column = self.find_last_unmatched_paren(line_to_cursor)
+      column = self.find_last_unmatched_open_paren(line_upto_cursor)
+      whitespace_region = self.expand_to_whitespace(selection.a)
+      view.erase(edit, whitespace_region)
       if column:
-        view.insert(edit, selection.a, '\n%s' % (' ' * column))
+        view.insert(edit, whitespace_region.a, '\n%s' % (' ' * column))
       else:
-        # Would be better to run built in 'insert' command as it has some
-        # heuristics also, but that would trigger for all selections so can't do.
-        view.insert(edit, selection.a, '\n')
+        # Would be better to run built in 'insert' command as it does some
+        # heuristics, but that would trigger for all selections so can't do.
+        view.insert(edit, whitespace_region.a, '\n')
 
-  def find_last_unmatched_paren(self, line):
+  def find_last_unmatched_open_paren(self, line):
+    '''Returns offset of the last unmatched opening parenthesis on the line'''
     line_length = len(line)
     closing_paren = 0
     while line_length > 0:
@@ -33,3 +36,18 @@ class IndentToParenthesisCommand(sublime_plugin.TextCommand):
           return line_length + 1
 
     return None
+
+  def expand_to_whitespace(self, point):
+    '''Returns region with all spaces matched before and after the point'''
+    view = self.view
+    line_upto_point = view.substr(sublime.Region(view.line(point).a, point))
+    whitespace_to_eat = 0
+    last_non_ws_index = len(line_upto_point)
+    while last_non_ws_index > 0:
+      last_non_ws_index -= 1
+      char = line_upto_point[last_non_ws_index]
+      if char != ' ':
+        break
+      whitespace_to_eat += 1
+
+    return view.find('\s*', point - whitespace_to_eat)
